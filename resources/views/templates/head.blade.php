21025533f4d02142ce30186f7bc6f3d268120b08
erikbocil <erikluis3006@gmail.com>
2022-08-15 17:08:07 +0700

    Tambah favicon dan ganti gambar navbar

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -4,6 +4,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('site.webmanifest')}}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-5">
     <div class="container-fluid">
-      <a class="navbar-brand" href="{{url('book')}}"><img src="https://c.tenor.com/woY-G8ICu-UAAAAC/kobo-kanaeru-hai.gif" alt="kobo" width="200px"></a>
+      <a class="navbar-brand" href="{{url('book')}}"><img src="{{asset('img/kobo-nobg.png')}}" alt="kobo" width="100px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
